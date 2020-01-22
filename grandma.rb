b1c5651b47fbdcde2f3a1307aa3736e9a277492d
David Winniford <dwinniford@gmail.com>
2020-01-22 17:14:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(greeting)
-  if greeting.upcase == greeting && greeting !== "I LOVE YOU GRANDMA!"
+  if greeting.upcase == greeting && !(greeting == "I LOVE YOU GRANDMA!")
     puts "NO, NOT SINCE 1938!"
   elsif greeting == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
